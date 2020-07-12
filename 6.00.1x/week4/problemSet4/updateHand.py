645a7549c8e3bcec32fa9acd77c2fe4c952378a3
Kem Crimmins <kem.crimmins@gmail.com>
2020-07-12 12:15:55 -0400

    add updateHand

---
+++ 6.00.1x/week4/problemSet4/updateHand.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 12 12:12:28 2020
+
+@author: kemcrimmins
+"""
+
+def updateHand(hand, word):
+    """
+    Assumes that 'hand' has all the letters in word.
+    In other words, this assumes that however many times
+    a letter appears in 'word', 'hand' has at least as
+    many of that letter in it. 
+
+    Updates the hand: uses up the letters in the given word
+    and returns the new hand, without those letters in it.
+
+    Has no side effects: does not modify hand.
+
+    word: string
+    hand: dictionary (string -> int)    
+    returns: dictionary (string -> int)
+    """
+    new_hand = hand.copy()
+    
+    for letter in word:
+        new_hand[letter] -= 1
+        
+    return new_hand
+    
