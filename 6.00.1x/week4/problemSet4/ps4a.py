3faf853adf0586d0f00d6d48c94afa17b316cfb7
Kem Crimmins <kem.crimmins@gmail.com>
2020-07-12 12:16:35 -0400

    add updateHand to game

--- 6.00.1x/week4/problemSet4/ps4a.py
+++ 6.00.1x/week4/problemSet4/ps4a.py
@@ -152,7 +152,12 @@ def updateHand(hand, word):
     hand: dictionary (string -> int)    
     returns: dictionary (string -> int)
     """
-    # TO DO ... <-- Remove this comment when you code this function
+    new_hand = hand.copy()
+    
+    for letter in word:
+        new_hand[letter] -= 1
+        
+    return new_hand
 
 
 
